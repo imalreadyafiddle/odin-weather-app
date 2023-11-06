552ad561cb0d1ec406ff341f6618f3aeb482959e
Christopher Davis <christopher.m.davis90@gmail.com>
2023-11-06 16:17:57 -0500

    basic async functions written, general layout 80% done

--- main.js
+++ main.js
@@ -1,1 +1,50 @@
 // weather functions go here
+const apiKey = "edeac25381f74542b78134423233110";
+const locationSearch = document.getElementById("locationSearch");
+let locationSearchValue = "Orlando";
+
+async function howzaNow() {
+  const response = await fetch(
+    "https://api.weatherapi.com/v1/current.json?key=" +
+      apiKey +
+      "&q=" +
+      locationSearchValue +
+      "&aqi=yes"
+  );
+  const nowData = await response.json();
+  console.log(nowData);
+  return nowData;
+}
+
+async function howzaThen() {
+  const response = await fetch(
+    "https://api.weatherapi.com/v1/forecast.json?key=" +
+      apiKey +
+      "&q=" +
+      locationSearchValue +
+      "&days=3&aqi=yes&alerts=yes"
+  );
+  const thenData = await response.json();
+  console.log(thenData);
+  return thenData;
+}
+
+async function howzaWhere() {
+  const response = await fetch(
+    "https://api.weatherapi.com/v1/search.json?key=" +
+      apiKey +
+      "&q=" +
+      locationSearchValue
+  );
+  const whereData = await response.json();
+  console.log(whereData);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  let searchButton = document.getElementById("searchGo");
+  searchButton.addEventListener("click", () => {
+    howzaNow();
+    howzaThen();
+    howzaWhere();
+  });
+});
